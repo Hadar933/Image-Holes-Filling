abdbca073d73c9460d3c7b1cec93e0c4dcc7e463
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2022-04-30 14:36:45 +0300

    basic functionality works

--- algo.py
+++ algo.py
@@ -1,11 +1,11 @@
 import numpy as np
 from typing import Callable
 import cv2 as cv
-from matplotlib import pyplot as plt
 from scipy.spatial.distance import cdist
 
 
-def generate_image_with_hole(image_filename: str = 'img.jpg', mask_filename: str = 'mask.jpg'):
+def generate_image_with_hole(image_filename: str = 'img.jpg',
+                             mask_filename: str = 'mask.jpg'):
     """
     As suggested, takes an image and a mask and merges then such that the mask is assigned with pixel value -1
     :param image_filename: path to the image file
@@ -25,9 +25,14 @@ def generate_image_with_hole(image_filename: str = 'img.jpg', mask_filename: str
     return merged
 
 
-def get_neighbours(im: np.ndarray, pixel: np.ndarray, connectivity: int):
+def get_neighbours(height: int,
+                   width: int,
+                   pixel: np.ndarray,
+                   connectivity: int):
     """
     an O(1) function that takes a pixel and returns its neighbours indexes (with edge cases)
+    :param width: input width
+    :param height: input height
     :param connectivity: either 4 or 8
     :param pixel: [x,y]
     :return: the neighbors of [x,y] based on the connectivity
@@ -38,32 +43,34 @@ def get_neighbours(im: np.ndarray, pixel: np.ndarray, connectivity: int):
     if connectivity == 4:
         points = points[:4]
     # this line takes care of edges:
-    points = [[x, y] for x, y in points if x != -1 and y != -1 and x != im.shape[0] + 1 and y != im.shape[1] + 1]
+    points = [[x, y] for x, y in points if x != -1 and y != -1 and x != width + 1 and y != height + 1]
     return np.array(points)
 
 
-def find_hole_and_boundary(I: np.ndarray, connectivity: int):
+def find_hole_and_boundary(I: np.ndarray,
+                           connectivity: int):
     """
     find the hole H and its boundary B in image I.
     :param connectivity: either 4 or 8
     :param I: the input grayscale image
     :return: Border (B) ,Hole (H)
     """
+    height, width = I.shape[0], I.shape[1]
     H = np.argwhere(I == -1)
     B = []
     for pixel in H:
-        N = get_neighbours(I, pixel, connectivity)
-        edges = [nei for nei in N if I[nei[0], nei[1]] >= 0]
-        B += edges
+        N = get_neighbours(height, width, pixel, connectivity)
+        edges = [nei for nei in N if I[nei[0], nei[1]] >= 0]  # all points not in H
+        B += edges  # appending to B with possible duplicates
     return np.unique(B, axis=0), H
 
 
-def fill_hole1(I: np.ndarray,
-               H: np.ndarray,
-               B: np.ndarray,
-               z: float,
-               eps: float,
-               w: [Callable[[np.ndarray, np.ndarray], float]]):
+def fill_hole(I: np.ndarray,
+              H: np.ndarray,
+              B: np.ndarray,
+              z: float,
+              eps: float,
+              w: [Callable[[np.ndarray, np.ndarray], float]]):
     """
     this function implements the suggested hole filling algorithm
     :param eps: tuning parameter to avoid zero division
@@ -81,43 +88,28 @@ def fill_hole1(I: np.ndarray,
     B_index = tuple(B.T)
     I[H_index] = (w_matrix.dot(I[B_index])) / (w_matrix.dot(np.ones(B.shape[0])))
     return I
+    # a non vectorized verision which is about 3 times slower may look something like this:
+    # for u in H:
+    #     I[u[0], u[1]] = sum([w(u, v) * I[v[0], v[1]] for v in B]) / sum([w(u, v) for v in B])
 
-
-def fill_hole2(I: np.ndarray, H: np.ndarray, B: np.ndarray, z, eps,
-               w: [Callable[[np.ndarray, np.ndarray], float]] =
-               lambda u, v, z, eps: 1 / (eps + np.linalg.norm(u - v) ** z)):
-    """
-    this function implements the suggested hole filling algorithm
-    :param I: given image
-    :param H: hole in the image
-    :param B: boundary of H
-    :param w: weight function
-    :return: image with filled hole
-    """
-    default_w = lambda u, v: 1 / (eps + np.linalg.norm(u - v) ** z)
-    # trivial solution:
-    for u in H:
-        I[u[0], u[1]] = sum([default_w(u, v) * I[v[0], v[1]] for v in B]) / sum([default_w(u, v) for v in B])
-    return I
-
-
-# %%
-im = generate_image_with_hole('monkey.jpg')
-# %%
-boundary, hole = find_hole_and_boundary(im, 8)
-# %%
+#
+# # %%
+# im = generate_image_with_hole('monkey.jpg')
+# # %%
+# boundary, hole = find_hole_and_boundary(im, 8)
+# # %%
 # plt.imshow(im, cmap='gray')
-# B_tup = tuple(np.array(boundary).T)
+# B_tup = tuple(boundary.T)
 # H_tup = tuple(hole.T)
 # plt.scatter(x=H_tup[1], y=H_tup[0], c='blue', s=1)
 # plt.scatter(x=B_tup[1], y=B_tup[0], c="red", s=1)
 # plt.show()
-# %%
-import time
-
-start = time.time()
-I1 = fill_hole1(im, hole, boundary, 2, 0.01, None)
-end = time.time()
-plt.imshow(I1, cmap='gray')
-plt.title(f"alg 1 - vectorized, took {end - start}")
-plt.show()
+# # %%
+# import time
+#
+# start = time.time()
+# I1 = fill_hole(im, hole, boundary, 2, 0.01, None)
+# end = time.time()
+# plt.imshow(I1, cmap='gray')
+# plt.title(f"alg 1 - vectorized, took {end - start}")
+# plt.show()
---
+++ main.py
@@ -0,0 +1,68 @@
+import argparse
+from os.path import exists
+from algo import generate_image_with_hole, find_hole_and_boundary, fill_hole
+from matplotlib import pyplot as plt
+from PIL import Image
+
+
+def check_file_path(filepath: str):
+    if not exists(filepath):
+        raise argparse.ArgumentTypeError(f'File {filepath} does not exists.')
+    return filepath
+
+
+def check_eps(eps):
+    eps = float(eps)
+    if eps <= 0:
+        raise argparse.ArgumentTypeError(f'Epsilon must be > 0.')
+    return eps
+
+
+def check_connect(c):
+    c = int(c)
+    if c not in [4, 8]:
+        raise argparse.ArgumentTypeError('The connectivity must be 4 or 8.')
+    return c
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Given an original image and a hole image, merges the two and fills the hole.'
+                    ' The result is saved as merged.jpg.'
+                    ' Use flag -h for more information, or simply run py main.py for default behaviour.'
+    )
+    parser.add_argument('image_file_path', metavar='image_path', type=check_file_path, nargs="?", const='img.jpg',
+                        default='img.jpg',
+                        help=f'Image file path (default=img.jpg).')
+    parser.add_argument('hole_file_path', metavar='hole_path', type=check_file_path, nargs="?", const='mask.jpg',
+                        default='mask.jpg',
+                        help=f'Mask file path (same size as image, otherwise reshaped, default=mask.jpg).')
+    parser.add_argument('z', metavar='z', type=float, nargs="?", const=2, default=2,
+                        help=f'Power term for the distance metric (default=2).')
+    parser.add_argument('epsilon', metavar='eps', type=check_eps, nargs="?", const=0.01, default=0.01,
+                        help=f'Small float value used to avoid division by zero (default=0.01).')
+    parser.add_argument('connectivity', metavar='connectivity', type=check_connect, nargs="?", const=4, default=4,
+                        help=f'Either 4 or 8 (default=4).')
+
+    args = parser.parse_args()
+    im_pth, msk_pth, z, eps, conn = args.image_file_path, args.hole_file_path, args.z, args.epsilon, args.connectivity
+
+    print("Merging image+mask...")
+    im = generate_image_with_hole(im_pth, msk_pth)
+
+    print("Finding hole+boundary...")
+    boundary, hole = find_hole_and_boundary(im, conn)
+
+    # plt.imshow(im, cmap='gray')
+    # B_tup = tuple(boundary.T)
+    # H_tup = tuple(hole.T)
+    # plt.scatter(x=H_tup[1], y=H_tup[0], c='blue', s=1)
+    # plt.scatter(x=B_tup[1], y=B_tup[0], c="red", s=1)
+    # plt.show()
+
+    print("Filling hole...")
+    I1 = fill_hole(im, hole, boundary, z, eps, None)
+
+    print("Saving result as merged.jpg...")
+    plt.imshow(I1, cmap='gray')
+    plt.savefig("merged.jpg")
